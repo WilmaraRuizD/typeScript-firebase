9f155f5df085a56c2d39e47c8deae63a02328995
Wilmara Ruiz <wilmara_andreina93@hotmail.com>
2022-05-20 12:42:35 -0500

    typescript-firebase-mongodb

---
+++ frontend/scripts/config/firebase-config.js
@@ -0,0 +1,11 @@
+export default{
+   
+        apiKey: "AIzaSyDglFnzVe0KieCk3oVztJ5STzm25hriIMs",
+        authDomain: "tiendas-js.firebaseapp.com",
+        projectId: "tiendas-js",
+        storageBucket: "tiendas-js.appspot.com",
+        messagingSenderId: "932747064564",
+        appId: "1:932747064564:web:b631ae0995960e64eb9aa1"
+   
+
+}
